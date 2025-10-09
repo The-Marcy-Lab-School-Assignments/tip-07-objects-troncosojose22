cd142f1f5c98cfca4f834b62c31ca1a8a9adc589
troncosojose22 <troncosojose22@gmail.com>
2025-10-09 12:59:27 -0400

    Tip example object example completed

--- example.js
+++ example.js
@@ -5,18 +5,20 @@ const movie = {
 };
 
 // Access values
-console.log(movie.title); // "Inception"
-console.log(movie["releaseYear"]); // 2010
+// console.log(movie.title); // "Inception"
+// console.log(movie["releaseYear"]); // 2010
 
-// Update a value
-movie.director = "Nolan";
-console.log(movie.director); // "Nolan"
+// // Update a value
+// movie.director = "Nolan";
+// // console.log(movie.director); // "Nolan"
 
-// Add a new key/value pair
-movie.rating = 9;
-console.log(movie.rating); // 9
+// // Add a new key/value pair
+// movie.rating = 9;
+// console.log(movie.rating); // 9
 
 // Loop through the object
-for (let key in movie) {
-  console.log(`${key}: ${movie[key]}`);
+let arr = [];
+for (const key in movie) {
+  arr.push(movie[key]);
 }
+console.log(arr);
