ce549ef52cd6b857ff51a563158ea14d56ec74e7
troncosojose22 <troncosojose22@gmail.com>
2025-10-10 10:35:03 -0400

    Completed tip 7 objects

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //write your code here
+const countWordFrequency = (sentence) => {
+    let words = sentence.split(" ");
+    let obj = {};
+
+    for (let i = 0; i < words.length; i++) {
+        if (obj[words[i]]) {
+            obj[words[i]] += 1;
+            continue;
+        }
+        obj[words[i]] = 1;
+    }
+    console.log(obj);
+} 
+countWordFrequency("the cat and the hat");
